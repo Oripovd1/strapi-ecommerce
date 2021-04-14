60fc0b9d55cef2a01ca4111dec645ded1b20b2dd
Oripovd1 <oripovd1@gmail.com>
2021-04-14 10:56:27 +0500

    file upload to s3 configured

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,13 @@
-// module.exports = ({ env }) => ({
-//   upload: {
-//     provider: "tp-minio",
-//     providerOptions: {
-//       accessKey: env("MINIO_ACCESS_KEY"),
-//       secretKey: env("MINIO_SECRET_KEY"),
-//       bucket: env("MINIO_BUCKET"),
-//       endPoint: env("MINIO_ENDPOINT"),
-//       port: parseInt(env("MINIO_PORT"), 10) || 9000,
-//       useSSL: env("MINIO_USE_SSL") === "true",
-//       folder: "cms",
-//       isDocker: true,
-//       host: env("MINIO_HOST"),
-//     },
-//   },
-// });
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: "us-east-2",
+      params: {
+        Bucket: "giotto-strapi",
+      },
+    },
+  },
+});
