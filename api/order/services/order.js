fb99f5ddc0d18bcac406a6406976e930dcc1374b
Oripovd1 <oripovd1@gmail.com>
2021-04-14 20:53:39 +0500

    order model changed

--- api/order/services/order.js
+++ api/order/services/order.js
@@ -1,8 +1,22 @@
-'use strict';
+const { isDraft } = require("strapi-utils").contentTypes;
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
+module.exports = {
+  /**
+   * Promise to add record
+   *
+   * @return {Promise}
+   */
 
-module.exports = {};
+  async create(data) {
+    const isDraftData = isDraft(data, strapi.models.order);
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models.order,
+      data,
+      { isDraftData }
+    );
+
+    const entry = await strapi.query("order").create(validData);
+
+    return entry;
+  },
+};
